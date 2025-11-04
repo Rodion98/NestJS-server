240165873682078661451df2d6f5133e0a631f09
Rodion98 <116246662+Rodion98@users.noreply.github.com>
2025-11-04 21:41:34 +0600

    Some fix

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,16 @@
 
 import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from './prisma-client-exception.filter.js';
 import { ConfigService } from '@nestjs/config';
 import type {
   CorsConfig,
   NestConfig,
-  SwaggerConfig,
 } from './common/configs/config.interface.js';
-
 import * as express from 'express';
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { join, } from 'path';
+import { setupSwagger } from './swagger/swagger.config.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -37,29 +31,17 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const nestConfig = configService.get<NestConfig>('nest');
   const corsConfig = configService.get<CorsConfig>('cors');
-  const swaggerConfig = configService.get<SwaggerConfig>('swagger');
 
-  // Swagger Api
-  if (swaggerConfig.enabled) {
-    const options = new DocumentBuilder()
-      .setTitle(swaggerConfig.title || 'Nestjs')
-      .setDescription(swaggerConfig.description || 'The nestjs API description')
-      .setVersion(swaggerConfig.version || '1.0')
-      .addBearerAuth()
-      .build();
-    const document = SwaggerModule.createDocument(app, options);
+   setupSwagger(app, configService);
 
-    SwaggerModule.setup(swaggerConfig.path || 'api', app, document);
-  }
-
-  // Cors
+   // Cors
   if (corsConfig.enabled) {
     app.enableCors();
   }
 
   await app.listen(process.env.PORT || nestConfig.port || 3000);
   console.log(`🚀 Server started on http://localhost:3000`);
-  console.log(`📘 Swagger on   http://localhost:3000/docs`);
+  console.log(`📘 Swagger on   http://localhost:3000/api`);
   console.log(`📘 AdminJS on   http://localhost:3000/admin`);
 }
 bootstrap();
